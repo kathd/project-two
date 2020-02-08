6fb4b90a65bcac9229aefdfebc97bcdb3641e286
Jon <orejajonathan@gmail.com>
2020-02-08 14:57:30 +0100

    server fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-
+require("dotenv").config();
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 

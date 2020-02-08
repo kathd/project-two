3ee4a5ccd836b2ac2e90a49a1ad29b270afcc21e
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-02-08 14:43:48 +0100

    fixed bug

--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ const userSchema = new Schema({
     },
     role: {
         type: String,
-        enum: ["admin", "user"]
+        enum: ["admin", "user"],
         default: "user"
     },
     avatar: {

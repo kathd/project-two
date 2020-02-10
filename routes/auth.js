c537c9e9246f95df467f8ddae482770709e9b14c
Jon <orejajonathan@gmail.com>
2020-02-10 10:08:44 +0100

    photographer route working

--- routes/auth.js
+++ routes/auth.js
@@ -6,11 +6,11 @@ const bcrypt = require("bcryptjs");
 
 //VIEW FORM SIGNIN/UP
 router.get("/signin", (req, res) => {
-  res.render("/auth/signin");
+  res.render("auth/signin");
 });
 
 router.get("/signup", (req, res) => {
-  res.render("/auth/signup");
+  res.render("auth/signup");
 });
 
 

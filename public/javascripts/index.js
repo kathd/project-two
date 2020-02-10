4df1b0d165def143f55ea68048667ff7854914fb
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-02-10 11:17:04 +0100

    index page + set masonry templates

---
+++ public/javascripts/index.js
@@ -0,0 +1,16 @@
+// ===== MASONRY =====
+
+// vanilla JS
+// init with element
+var grid = document.querySelector('.grid');
+var msnry = new Masonry( grid, {
+  // options...
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
+
+// init with selector
+var msnry = new Masonry( '.grid', {
+  // options...
+});
+

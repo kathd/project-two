4e78dcb408d9886a20142e5288b8c1e5188234f9
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-02-14 00:07:30 +0100

    signin signup pages made responsive

---
+++ public/javascripts/nav.js
@@ -0,0 +1,5 @@
+if (window.matchMedia("(max-width: 700px)").matches) {
+    /* The viewport is less than, or equal to, 700 pixels wide */
+  } else {
+    /* The viewport is greater than 700 pixels wide */
+  }

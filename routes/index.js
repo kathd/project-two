44888e8b186a097b176dfb6a14f2aebe44813c42
Jon <orejajonathan@gmail.com>
2020-02-14 11:50:55 +0100

    route secure and admin protection

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 const userModel = require('../models/User');
 const photographerModel = require('../models/Photographer');
 const uploader = require("./../config/cloudinary");
+const protectUserRoute = require("../middlewares/protectUserRoute");
+const protectAdminRoute = require("../middlewares/protectAdminRoute");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,7 +24,7 @@ router.get('/', function(req, res, next) {
   .catch(err => console.error(err))
 });
 
-router.get('/account', function(req, res, next) {
+router.get('/account', protectUserRoute, function(req, res, next) {
   userModel
   .findById(req.session.currentUser._id)
   .populate('photogfav')
@@ -34,7 +37,7 @@ router.get('/account', function(req, res, next) {
   .catch(err => console.error("OH no, db err :", err));
 });
 
-router.post('/account/:id/remove', function(req, res, next) {
+router.post('/account/:id/remove', protectUserRoute, function(req, res, next) {
   photographerModel
   .findByIdAndUpdate(req.params.id, { $pull: { fans : req.session.currentUser._id}}, {new:true})
   .then(photographer => { 
@@ -51,7 +54,7 @@ router.post('/account/:id/remove', function(req, res, next) {
 })
 
 
-router.get('/account/edit', function(req, res, next) {
+router.get('/account/edit', protectUserRoute, function(req, res, next) {
   userModel
   .findById(req.session.currentUser._id)
   .then(user => {
